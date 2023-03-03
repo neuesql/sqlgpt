5ced07fc5611084d57503b3fa1eaec9eb9a5c8dc
Qunfei Wu <qunfei.wu@allianz.com>
2023-03-03 21:42:53 +0100

    delete the data set ,moving it into s3

--- app.py
+++ app.py
@@ -1,76 +1,45 @@
-import torch
-from metaflow import FlowSpec, step
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title('Uber pickups in NYC')
+import streamlit as st
+import pandas as pd
+from io import StringIO
 
-class TrainingFlow(FlowSpec):
+databases = ['Oracle', 'DB2', 'SQLServer', 'PostgreSQL']
 
-    @step
-    def start(self):
-        print("Start building model job")
-        self.next(self.preparing)
+source_database = st.sidebar.selectbox(
+    label='Source Database',
+    options=databases,
+    index=0
+)
 
-    @step
-    def end(self):
-        print("End building job")
+target_database = st.sidebar.selectbox(
+    label='Target Database',
+    options=databases,
+    index=3
+)
 
-    @step
-    def preparing(self):
-        self.next(self.training)
+input_files = st.sidebar.file_uploader("Choose a SQL file", accept_multiple_files=True)
+for uploaded_file in input_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
 
-    @step
-    def training(self):
-        self.next(self.validation)
+input_text = st.sidebar.text_area(
+    label='Insert SQL',
+    placeholder='select * from now()'
+)
+transform_button = st.sidebar.button(
+    label='Transform', type='primary')
 
-    @step
-    def validation(self):
-        self.next(self.end)
+col1, col2 = st.columns(2)
 
-    def train_process(self, epoch, tokenizer, model, device, loader, optimizer):
-        model.train()
-        for _, data in enumerate(loader, 0):
-            y = data['target_ids'].to(device, dtype=torch.long)
-            ids = data['source_ids'].to(device, dtype=torch.long)
-            mask = data['source_mask'].to(device, dtype=torch.long)
+with col1:
+    st.header("Source SQL")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-            outputs = model(input_ids=ids, attention_mask=mask, labels=y)
-            loss = outputs[0]
-
-            if _ % 500 == 0:
-                print(f'Epoch: {epoch}, Loss:  {loss.item()}')
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-    def validate_process(self, epoch, tokenizer, model, device, loader):
-        model.eval()
-        predictions = []
-        actuals = []
-        with torch.no_grad():
-            for _, data in enumerate(loader, 0):
-                y = data['target_ids'].to(device, dtype=torch.long)
-                ids = data['source_ids'].to(device, dtype=torch.long)
-                mask = data['source_mask'].to(device, dtype=torch.long)
-
-                generated_ids = model.generate(
-                    input_ids=ids,
-                    attention_mask=mask,
-                    max_length=150,
-                    num_beams=2,
-                    repetition_penalty=2.5,
-                    length_penalty=1.0,
-                    early_stopping=True
-                )
-                preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in
-                         generated_ids]
-                target = [tokenizer.decode(t, skip_special_tokens=True, clean_up_tokenization_spaces=True) for t in y]
-                if _ % 100 == 0:
-                    print(f'Completed {_}')
-
-                predictions.extend(preds)
-                actuals.extend(target)
-        return predictions, actuals
-
-
-if __name__ == "__main__":
-    flow = TrainingFlow()
+with col2:
+    st.header("Target SQL")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
